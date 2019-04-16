1aa491d27d8539b086cc70c4d3808334c37110cc
mhsDYoungman <48356531+mhsDYoungman@users.noreply.github.com>
2019-04-16 09:20:09 -0400

    Add files via upload

---
+++ using the mouse/sketch.js
@@ -0,0 +1,30 @@
+let width = 20
+let height = 40
+let m = 1.2
+function setup() {
+createCanvas(1280, 877.49);
+}
+
+function draw() {
+
+}
+
+function mouseMoved() {
+  background(0)
+  rectMode(CENTER)
+  rect(mouseX, mouseY, width, height);
+}
+
+function mouseClicked() {
+  fill(random(255), random(255), random(255));
+
+    if(width<20*m*35) {
+    width=width*m
+    height=height*m/1.06
+  }
+    if (width>=20*m*35) {
+    width = 20
+    height = 40
+    }
+
+}
