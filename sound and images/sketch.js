2e4e81a6098113c8ac57d27847c4036c02fcfaaf
mhsDYoungman <48356531+mhsDYoungman@users.noreply.github.com>
2019-04-24 08:35:46 -0400

    Add files via upload

---
+++ sound and images/sketch.js
@@ -0,0 +1,12 @@
+var mySound; // Creates a variable to hold the sound
+
+ function preload() {
+   mySound = loadSound('We Wish You.mp3');  // Loads the sound file into the variable
+ }
+
+ function setup() {
+   createCanvas(1280, 877.49);
+   // Notice that you have to put the variable name in front of the functions
+   mySound.setVolume(0.1);
+   mySound.play();
+ }
