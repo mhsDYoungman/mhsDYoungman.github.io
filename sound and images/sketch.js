d0d57addbfc83b6bbb5b4a6cee8d7b802fc59fee
mhsDYoungman <48356531+mhsDYoungman@users.noreply.github.com>
2019-04-25 10:14:50 -0400

    Update sketch.js

--- sound and images/sketch.js
+++ sound and images/sketch.js
@@ -1,23 +1,22 @@
-let mySound; // Creates a variable to hold the sound
+let mySound // Creates a variable to hold the sound
 
  function preload() {
-   mySound = loadSound('tasmanianDevilDanielSimon.mp3');  // Loads the sound file into the variable
+   mySound = loadSound('tasmanianDevilDanielSimon.mp3') // Loads the sound file into the variable
+  img = loadImage('assets/laDefense.jpg')
  }
 
  function setup() {
-   createCanvas(1280, 877.49);
-   // Notice that you have to put the variable name in front of the functions
-   //mySound.setVolume(0.1);
-   //mySound.play();
+   createCanvas(1280, 877.49)
+  image(img, 0, 0)
  }
 
 function mousePressed() {
   if (mySound.isPlaying()) {
-    mySound.stop();
-    background(255, 0, 0);
+    mySound.stop()
+    background(255, 0, 0)
   } else {
-    mySound.play();
-    background(0, 255, 0);
+    mySound.play()
+    background(0, 255, 0)
   }
 }
 
