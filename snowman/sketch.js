b8a76e56955d0f9460fd9dc504475bda4ca3ed76
mhsDYoungman <48356531+mhsDYoungman@users.noreply.github.com>
2019-05-08 08:45:01 -0400

    Update sketch.js

--- snowman/sketch.js
+++ snowman/sketch.js
@@ -6,19 +6,19 @@ function setup() {
 function draw() {
   // put drawing code here
   // base;
-ellipse(200, 293, 140, 140);
+  ellipse(200, 293, 140, 140);
 
-// middle;
-ellipse(200, 200, 100, 100);
+  // middle;
+  ellipse(200, 200, 100, 100);
 
-// top;
-ellipse(200, 136, 75, 75);
+  // top;
+  ellipse(200, 136, 75, 75);
 
-// ground;
-rectMode(CENTER);
-rect(200, 389, 390, 50);
+  // ground;
+  rectMode(CENTER);
+  rect(200, 389, 390, 50);
 
-// arms;
-line(250, 200, 300, 175);
-line(150, 200, 100, 175);
+  // arms;
+  line(250, 200, 300, 175);
+  line(150, 200, 100, 175);
 }
